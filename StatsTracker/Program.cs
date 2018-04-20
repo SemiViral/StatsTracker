575a5eafcb5e4d462378ba3f08abd2b9c33f8ee5
Antonio DiNostri <semiviral@gmail.com>
2018-04-19 22:22:36 -0500

    added tables list

--- StatsTracker/Program.cs
+++ StatsTracker/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using StatsTracker.Model;
 
 namespace StatsTracker {
     public class Program {
         public static void Main(string[] args) {
             Console.WriteLine("Loading all statistic tables.");
+
+            // todo write code to load tables from database/file
+            Tables = new Dictionary<string, StatTable>();
         }
 
         public static string GetUserInput() {
@@ -15,5 +20,7 @@ namespace StatsTracker {
 
             return rawInput;
         }
+
+        private static Dictionary<string, StatTable> Tables { get; set; }
     }
 }
