6d2ddfdff4b57ef35e50527135cb7fe5d7694318
Antonio DiNostri <semiviral@gmail.com>
2018-04-24 00:01:07 -0500

    implemented GetTable() method
    changed Tables property to a List<StatTable>

--- StatsTracker/Program.cs
+++ StatsTracker/Program.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using StatsTracker.Model;
 
 namespace StatsTracker {
     public class Program {
         #region MEMBERS
 
-        private static Dictionary<string, StatTable> Tables { get; set; }
+        private static List<StatTable> Tables { get; set; }
 
         #endregion
 
         public static void Main(string[] args) {
-            Tables = new Dictionary<string, StatTable>();
+            Tables = new List<StatTable>();
 
             Console.WriteLine("Loading all statistic tables.");
 
             // todo write code to load tables from database/file
-            Tables.Add("Basic", new StatTable("Basic"));
-            Tables["Basic"].StatisticLines = new List<StatLine> {new StatLine(new List<Stat> {new Stat<int>(0, "BasicStat01", 1)})};
+            Tables.Add(new StatTable("Basic"));
+            Tables[0].StatisticLines = new List<StatLine> {new StatLine(new List<Stat> {new Stat<int>(0, "BasicStat01", 1)})};
         }
 
         public static string GetUserInput() {
@@ -30,7 +31,7 @@ namespace StatsTracker {
             return rawInput;
         }
 
-        public static void ParseUserInput(string userInput) {
+        private static void ParseUserInput(string userInput) {
             string[] userInputSplit = userInput.Split(" ");
 
             switch (userInputSplit[0]) {
@@ -45,6 +46,18 @@ namespace StatsTracker {
                 default:
                     break;
             }
+        }
+
+        private static void CreateTable(string inputValues) {
+
+        }
+
+        private static void DeleteTable(StatTable table) {
+
+        }
+
+        private static StatTable GetTable(string tableName) {
+            return Tables.SingleOrDefault(table => table.Name.Equals(tableName));
         }
     }
 }
