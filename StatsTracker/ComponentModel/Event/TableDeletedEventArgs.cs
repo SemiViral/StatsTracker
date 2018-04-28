a9eb2711b6370a8e4df52c60069f850652e406a4
Antonio DiNostri <semiviral@gmail.com>
2018-04-27 20:22:50 -0500

    implemented TableDeletedEventArgs

--- StatsTracker/ComponentModel/Event/TableDeletedEventArgs.cs
+++ StatsTracker/ComponentModel/Event/TableDeletedEventArgs.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using StatsTracker.Model;
 
 namespace StatsTracker.ComponentModel.Event
 {
-    class TableDeletedEventArgs
-    {
+    public class TableDeletedEventArgs {
+        public StatTable Table { get; }
+
+        public TableDeletedEventArgs(StatTable table) {
+            Table = table;
+        }
     }
 }
